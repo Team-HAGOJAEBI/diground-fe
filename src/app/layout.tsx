0ed1a562f23db97fcbd326422e27f4c3518dae6a
9yurilee <9yurilee@gmail.com>
2025-07-15 17:43:32 +0900

    style: font 적용 방식 Next localFont로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import "./globals.css";
 import { MSWProvider } from "./providers";
+import localFont from "next/font/local";
+
+const Pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  style: "normal",
+  variable: "--font-pretendard",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html
+      lang="ko"
+      className={Pretendard.className}
+    >
       <body>
         <MSWProvider>{children}</MSWProvider>
       </body>
