b2fb8ff8dad275ade52e094b79bf8d8e7658f539
9yurilee <9yurilee@gmail.com>
2025-07-18 10:51:21 +0900

    style: 상세페이지 Layout 및 Footer 적용

---
+++ src/app/detail/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Footer from "../_common/Footer";
+import Layout from "../_common/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Footer selectedIndex={0} />
+    </Layout>
+  );
+}
