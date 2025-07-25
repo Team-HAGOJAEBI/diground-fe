92969d96d7b1220c932c9d9f62feda19626c5930
9yurilee <9yurilee@gmail.com>
2025-07-25 13:53:23 +0900

    refactor: width 360~1024px로 반응형 수정

--- src/app/_common/Layout.tsx
+++ src/app/_common/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 export default function Layout({ children, className }: LayoutProps) {
   return (
     <div className="h-screen w-screen bg-gray-5 flex items-center justify-center">
-      <div className={`w-[clamp(360px,360px,1024px)] h-[screen] bg-gray-5 ${className}`}>{children}</div>
+      <div className={`w-full max-w-[1024px] h-[screen] bg-gray-5 lg:mx-auto ${className}`}>{children}</div>
     </div>
   );
 }
