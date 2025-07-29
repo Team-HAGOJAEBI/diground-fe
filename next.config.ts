6df04d47d9694207e8b24ed1ec67c74d3184e6a8
9yurilee <9yurilee@gmail.com>
2025-07-29 22:05:36 +0900

    chore: 외부 이미지 도메인 허용 설정 속성 키 변경(deprecated)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // 외부 이미지 도메인 허용 설정
   images: {
-    domains: ["cdnimg.melon.co.kr"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdnimg.melon.co.kr",
+        port: "", // 사용하지 않으면 빈 문자열
+        pathname: "/**", // 서브 경로 전체 허용 (필터링이 필요하면 세부 경로로 좁혀도 됨)
+      },
+    ],
   },
 
   turbopack: {
