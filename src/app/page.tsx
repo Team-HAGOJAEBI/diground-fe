6eb40682f943f0ea54874916175ee34f890a86fd
9yurilee <9yurilee@gmail.com>
2025-08-01 16:11:34 +0900

    style: 테스트를 위한 메인페이지 UI 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import "../assets/styles/globals.css";
 
-import { useState } from "react";
-
-import { useAtom } from "jotai";
-
-import { playlistAtom } from "@/atoms";
-
 export default function Home() {
-  const [text, setText] = useState<string>("");
-  const [playlist, setPlaylist] = useAtom(playlistAtom);
+  const router = useRouter();
+
+  const mainMessage = "아직 로그인 전....";
 
   return (
-    <div>
-      <main>
-        <span className="bg-yellow-60 h-10 w-full text-gray-50">tailwindcss test</span>
-      </main>
-      <footer>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          className="mr-4 h-10 w-auto rounded-md border border-gray-300 p-2"
-        />
-        <button onClick={() => setPlaylist(text)}>결과 보기</button>
-        <span>결과: {playlist}</span>
-      </footer>
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-gray-100">
+      <p>{mainMessage}</p>
+      <button onClick={() => router.push("/login")}>로그인 하러 가기</button>
     </div>
   );
 }
