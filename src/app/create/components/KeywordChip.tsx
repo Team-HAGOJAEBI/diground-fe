9328f93019c02c9712e2eb2cad63649b9c19e411
chb6734 <chb6734@gmail.com>
2025-08-02 16:20:37 +0900

    style: KeyWordChip 컴포넌트 개발중...

--- src/app/create/components/KeywordChip.tsx
+++ src/app/create/components/KeywordChip.tsx
@@ -10,7 +10,7 @@ interface KeywordChipProps {
 
 export default function KeywordChip({
   keyword,
-  isSelected = false,
+
   onClick,
   disabled = false,
   variant = "default",
@@ -22,13 +22,20 @@ export default function KeywordChip({
   };
 
   return (
-    <button
-      className={`keyword-chip ${isSelected ? "selected" : ""} ${variant}`}
-      onClick={handleClick}
-      disabled={disabled}
-      type="button"
+    <div
+      className="relative flex w-full flex-[0_0_auto] flex-wrap items-start self-stretch"
+      role="group"
+      aria-label="키워드 선택"
     >
-      {variant === "add" ? "+" : keyword?.label}
-    </button>
+      <button
+        className={`bg-gray-5 relative inline-flex h-[50px] flex-[0_0_auto] flex-col items-center justify-center overflow-hidden rounded-[90px] border border-solid border-gray-50 p-20`}
+        // className={`keyword-chip ${isSelected ? "selected" : ""} ${variant}`}
+        onClick={handleClick}
+        disabled={disabled}
+        type="button"
+      >
+        {variant === "add" ? "+" : keyword?.label}
+      </button>
+    </div>
   );
 }
