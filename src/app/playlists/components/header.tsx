fbd32f914075593cdb138280e2c6530abad3c5a0
zenna <cheueun3@gmail.com>
2025-08-03 22:51:05 +0900

    feat : 지금 인기있는 플레이리스트, 내플리 없는 페이지

---
+++ src/app/playlists/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div
+      id="playlistsPageHeader"
+      className="absolute top-[20px] left-[20px] flex h-[35px] w-[160px] flex-col justify-center gap-[4px]"
+    >
+      <div className="h-[14px] text-[12px] font-[400] text-gray-50">좋아하는 것을, 더 좋아하도록</div>
+      <div className="text-yellow-80 h-[21px] justify-center text-[15px] font-[700]">DIGROUND</div>
+    </div>
+  );
+}
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,4 @@
 export { default as cover } from "./cover.jpg";
 export { default as defaultProfile } from "./defaultProfile.png";
 export { default as defaultProfile2x } from "./defaultProfile@2x.png";
+export { default as playArt } from "./playArt.png";
