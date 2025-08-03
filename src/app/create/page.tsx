b82a1abb6390e99f9d2ddd57b6c95525a2722cf2
chb6734 <chb6734@gmail.com>
2025-08-03 20:12:51 +0900

    style: 레이아웃 조정 및 스크롤 적용

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,13 +2,10 @@
 
 import { useCallback, useEffect, useRef, useState } from "react";
 
-import { getKeywordList } from "@/app/_common/api/keywordListApi";
 import Footer from "@/app/_common/Footer";
 import Layout from "@/app/_common/Layout";
 import CommonInput from "@/app/create/components/CommonInput";
 import KeywordChips from "@/app/create/components/KeywordChips";
-
-import type { Keyword } from "@/app/create/types/keyword";
 
 const SCROLL_TRIGGER = 200;
 const HEADER_MIN_HEIGHT = 60; // prev 아이콘 height + padding-top
@@ -72,48 +69,19 @@ function useHeaderAnimation(scrollRef: React.RefObject<HTMLDivElement | null>) {
   return { style, scrolling } as const;
 }
 
-interface FormData {
-  link: string;
-  title: string;
-  desc: string;
-  selectedKeywords: Keyword[];
-}
-
 export default function Page() {
-  const [formData, setFormData] = useState<FormData>({
-    link: "",
-    title: "",
-    desc: "",
-    selectedKeywords: [],
-  });
-  const [keywords, setKeywords] = useState<Keyword[]>([]);
-
-  const updateFormData = (field: keyof FormData, value: any) => {
-    setFormData((prev) => ({ ...prev, [field]: value }));
-  };
+  const [link, setLink] = useState("");
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
 
   const getValueError = () => {
-    if (!formData.link) return "링크를 복사하여 입력해주세요";
+    if (!link) return "링크를 복사하여 입력해주세요";
 
     return "";
   };
 
   const scrollRef = useRef<HTMLDivElement | null>(null);
   const { scrolling } = useHeaderAnimation(scrollRef);
-
-  useEffect(() => {
-    const fetchKeywords = async () => {
-      try {
-        const response = await getKeywordList();
-
-        setKeywords(response.data);
-      } catch (error) {
-        console.error("이것은 MSW에서 문제가 생긴듯 하다", error);
-      }
-    };
-
-    fetchKeywords();
-  }, []);
 
   return (
     <Layout className="relative flex h-screen flex-col overflow-hidden px-[56px] pt-[50px] pb-[89px]">
@@ -125,31 +93,90 @@ export default function Page() {
           플레이리스트 만들기
         </h1>
         <CommonInput
-          label="플랫폼 링크"
+          label="플렛폼 링크"
           required
           placeholder="유튜브 링크를 한번 넣어볼까나"
-          value={formData.link}
-          onChange={(e) => updateFormData("link", e.target.value)}
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
           errorMessage={getValueError()}
         />
         <CommonInput
           label="제목"
           placeholder="플레이리스트 제목을 입력해주세요."
-          value={formData.title}
-          onChange={(e) => updateFormData("title", e.target.value)}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-
         <CommonInput
           label="설명"
           placeholder="선택사항입니다."
-          value={formData.desc}
-          onChange={(e) => updateFormData("desc", e.target.value)}
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+          textarea
+        />
+        <CommonInput
+          label="설명"
+          placeholder="선택사항입니다."
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+          textarea
+        />
+        <CommonInput
+          label="설명"
+          placeholder="선택사항입니다."
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
           textarea
         />
         <KeywordChips
           title="# 키워드 선택"
-          keywords={keywords}
-          showAddButton={true}
+          keywords={[
+            {
+              id: 1,
+              label: "첫사랑",
+              category: "test",
+            },
+            {
+              id: 2,
+              label: "인디밴드",
+              category: "test",
+            },
+            {
+              id: 3,
+              label: "잔잔한",
+              category: "test",
+            },
+            {
+              id: 4,
+              label: "락",
+              category: "test",
+            },
+            {
+              id: 5,
+              label: "여름",
+              category: "test",
+            },
+            {
+              id: 6,
+              label: "겨울",
+              category: "test",
+            },
+            {
+              id: 7,
+              label: "이별",
+              category: "test",
+            },
+            {
+              id: 8,
+              label: "드라이브",
+              category: "test",
+            },
+            {
+              id: 9,
+              label: "여행",
+              category: "test",
+            },
+          ]}
+          // showAddButton={true}
           // onKeywordClick={(keyword) => console.log(keyword)}
           // onAddClick={() => console.log("Add clicked")}
         />
