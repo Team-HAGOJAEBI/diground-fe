8a7c2b53968c0a48973e334cb153673804ab1014
9yurilee <9yurilee@gmail.com>
2025-08-04 15:07:49 +0900

    feat: 소셜로그인 완료 테스트용 문구 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,61 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+
+import { User } from "@/mocks/sample/User";
+
 import "../assets/styles/globals.css";
 
 export default function Home() {
   const router = useRouter();
 
-  const mainMessage = "아직 로그인 전....";
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+
+  // 📑 임시
+  useEffect(() => {
+    const sessionUser = sessionStorage.getItem("user");
+
+    if (sessionUser) {
+      const { profile } = JSON.parse(sessionUser).kakao_account;
+
+      setUserInfo(profile);
+    } else {
+      setUserInfo(null);
+    }
+  }, []);
+
+  const userProfile = (user: User | null) => {
+    if (!user) {
+      return (
+        <div>
+          <p>로그인 전!!! 냅다 로그인하세요!</p>
+          <button onClick={() => router.push("/login")}>로그인 하러 가기</button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="flex flex-col items-center gap-2">
+        {user.profile_image_url ? (
+          <Image
+            src={user.profile_image_url}
+            alt="profile"
+            width={100}
+            height={100}
+          />
+        ) : null}
+        <p className="text-center">{user.nickname}님, 안녕하세요</p>
+      </div>
+    );
+  };
+  // 임시 끝
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center text-gray-100">
-      <p>{mainMessage}</p>
-      <button onClick={() => router.push("/login")}>로그인 하러 가기</button>
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-center text-gray-100">
+      {userProfile(userInfo)}
     </div>
   );
 }
