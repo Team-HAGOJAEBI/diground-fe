1839f7b240a8b2b85cb27d51e4235400795c94ba
chb6734 <chb6734@gmail.com>
2025-08-11 02:03:57 +0900

    feat: 카카오 로그인 구현

--- auth.ts
+++ auth.ts
@@ -1,9 +1,23 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
 
 import { prisma } from "./prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  useSecureCookies: process.env.Node_ENV === "production",
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {},
+  callbacks: {},
 });
