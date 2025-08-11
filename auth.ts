30f6a4422b75f6be1f6015c7bf6874c3cce8fbf9
chb6734 <chb6734@gmail.com>
2025-08-12 01:37:03 +0900

    feat: jwt 인코드 디코드 설정

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import * as jwt from "jsonwebtoken";
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 import KakaoProvider from "next-auth/providers/kakao";
 
 import { prisma } from "./prisma";
@@ -18,17 +20,25 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  jwt: {
+    encode: async ({ token, secret }) => {
+      return jwt.sign(token as jwt.JwtPayload, secret as string);
+    },
+    decode: async ({ token, secret }) => {
+      return jwt.verify(token as string, secret as string) as JWT;
+    },
+  },
   pages: {},
   callbacks: {
-    async signIn({ user, account, profile }) {
-      if (account?.provider === "kakao" && profile) {
-        // 카카오에서 email을 제공하지 않는 경우 기본값 설정
-        if (!user.email) {
-          user.email = `kakao_${profile.id}@diground.local`;
-        }
-      }
-
-      return true;
-    },
+    // async signIn({ user, account, profile }) {
+    //   if (account?.provider === "kakao" && profile) {
+    //     // 카카오에서 email을 제공하지 않는 경우 기본값 설정
+    //     if (!user.email) {
+    //       user.email = `kakao_${profile.id}@diground.local`;
+    //     }
+    //   }
+    //
+    //   return true;
+    // },
   },
 });
