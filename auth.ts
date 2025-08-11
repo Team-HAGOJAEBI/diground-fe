ea38bd9e4a56de72d80f02ed6d2e86fa6e0f3784
chb6734 <chb6734@gmail.com>
2025-08-11 23:22:43 +0900

    feat: 카카오 인증 관련 세팅 및 카카오에서 이메일(필수값)을 제공하지 않는 경우 설정(TEST)

--- auth.ts
+++ auth.ts
@@ -11,13 +11,24 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET,
   providers: [
     KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET,
     }),
   ],
   session: {
     strategy: "jwt",
   },
   pages: {},
-  callbacks: {},
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account?.provider === "kakao" && profile) {
+        // 카카오에서 email을 제공하지 않는 경우 기본값 설정
+        if (!user.email) {
+          user.email = `kakao_${profile.id}@diground.local`;
+        }
+      }
+
+      return true;
+    },
+  },
 });
