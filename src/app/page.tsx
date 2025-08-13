39154026efc3704812b024de8f7b95ca5cfad460
9yurilee <9yurilee@gmail.com>
2025-08-13 15:05:52 +0900

    remove: 기존 로그인(/login) 컴포넌트 작업 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-
 import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 import "../assets/styles/globals.css";
 
@@ -11,17 +8,12 @@ export default function Home() {
   const router = useRouter();
   const { status } = useSession();
 
-  useEffect(() => {
-    // 로딩이 완료된 후에만 리다이렉트
-    if (status !== "loading") {
-      // 로그인 여부와 상관없이 /playlists로 리디렉트
-      router.replace("/playlists");
-    }
-  }, [status, router]);
-
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center text-center text-gray-100">
-      <p className="mt-4">리다이렉트 중...</p>
+      <div>
+        <p>로그인 전!!! 냅다 로그인하세요!</p>
+        <button onClick={() => router.push("/signin")}>로그인 하러 가기</button>
+      </div>
     </div>
   );
 }
