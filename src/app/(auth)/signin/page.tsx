9637a2d14175dc334cd4d09cd880795c1b4ca071
9yurilee <9yurilee@gmail.com>
2025-08-13 15:12:09 +0900

    style: /signin에 로그인 UI 적용

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,16 +1,35 @@
 "use client";
 
+import Image from "next/image";
 import { signIn } from "next-auth/react";
 
-export default function SigninPage() {
+import Layout from "@/app/_common/Layout";
+import { kakaoLogin } from "@/assets/icons";
+
+export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <button
-        className="bg-yellow-80 rounded px-6 py-3"
-        onClick={() => signIn("kakao")}
-      >
-        Sign in with KaKao
-      </button>
-    </div>
+    <Layout className="flex h-[100vh] flex-col items-center justify-center text-center text-gray-100">
+      {/* Login Title */}
+      <div className="mb-[172px]">
+        <p>좋아하는 것을, 더 좋아하도록</p>
+        <span>DIGROUND</span>
+      </div>
+
+      {/* Login */}
+      <div>
+        <p>
+          간편하게 로그인하고 <br /> 다양한 노래를 DIGGING해 보세요.
+        </p>
+        <div className="flex">
+          <Image
+            src={kakaoLogin}
+            alt="카카오 로그인"
+            onClick={() => signIn("kakao", { redirectTo: "/detail" })}
+          />
+          <div>구글로 로그인</div>
+        </div>
+        <span>로그인 없이 둘러보기</span>
+      </div>
+    </Layout>
   );
 }
