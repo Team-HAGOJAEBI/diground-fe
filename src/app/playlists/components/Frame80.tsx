74169eb18d14ccef602ee696c8226f16fb0e16da
zenna <cheueun3@gmail.com>
2025-08-15 02:29:19 +0900

    feat: 인기있는 플레이리스트 클릭이벤트 추가

--- src/app/playlists/components/Frame80.tsx
+++ src/app/playlists/components/Frame80.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { readableColor } from "polished";
 
 import DigCount from "./Digcount";
@@ -18,47 +19,57 @@ export default function Frame80({
   shareCount: string;
   pliArt: { id: number; url: string; dominantColor: string };
 }) {
+  const router = useRouter();
   const textColor = readableColor(pliArt.dominantColor); // 도미넌트 컬러에 따라 텍스트 색상 결정
 
+  function handleClick(pliArtId: number) {
+    router.push(`/detail?${pliArtId}`); // 해당 플레이리스트로 이동
+  }
+
   return (
-    <div className="relative h-[226px] w-[190px]">
-      <div className="relative h-[190px] w-[190px]">
-        <Image
-          src={playArt}
-          alt={`${title}의 썸네일`}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-t-[16px]"
-        />
-      </div>
-      <div
-        className={`absolute top-[120px] h-[106px] w-[190px] rounded-b-[16px]`}
-        style={{
-          background: `linear-gradient(to top, ${pliArt.dominantColor} 70%, transparent 100%)`,
-        }}
-      >
-        <div className="absolute top-[24px] left-[10px] flex h-[72px] w-[170px] flex-col gap-[6px]">
-          <div
-            className={`line-clamp-2 h-[40px] w-[170px] text-[16px] leading-[20px] font-[700] ${
-              title.length <= 20 ? "flex items-center" : ""
-            }`}
-            style={{ color: textColor }}
-          >
-            {title}
+    <button
+      className="relative h-[226px] w-[190px]"
+      onClick={() => handleClick(pliArt.id)}
+    >
+      <div className="h-[226px] w-[190px]">
+        <div className="relative h-[190px] w-[190px]">
+          <Image
+            src={playArt}
+            alt={`${title}의 썸네일`}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-t-[16px]"
+          />
+        </div>
+        <div
+          className={`absolute top-[120px] h-[106px] w-[190px] rounded-b-[16px]`}
+          style={{
+            background: `linear-gradient(to top, ${pliArt.dominantColor} 70%, transparent 100%)`,
+          }}
+        >
+          <div className="absolute top-[24px] left-[10px] flex h-[72px] w-[170px] flex-col gap-[6px]">
+            <div
+              className={`line-clamp-2 h-[40px] w-[170px] text-[16px] leading-[20px] font-[700] ${
+                title.length <= 20 ? "flex items-center" : ""
+              }`}
+              style={{ color: textColor }}
+            >
+              {title}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="bg-gray-10 absolute bottom-[10px] left-[10px] mb-[1px] flex h-[26px] w-auto items-center justify-center rounded-[28px] p-[4px_8px_4px_6px]">
-        <DigCount
-          count={digCount}
-          iconName={"share"}
-        />
-        <DigCount
-          count={shareCount}
-          iconName={"share"}
-        />
+        <div className="bg-gray-10 absolute bottom-[10px] left-[10px] mb-[1px] flex h-[26px] w-auto items-center justify-center rounded-[28px] p-[4px_8px_4px_6px]">
+          <DigCount
+            count={digCount}
+            iconName={"share"}
+          />
+          <DigCount
+            count={shareCount}
+            iconName={"share"}
+          />
+        </div>
       </div>
-    </div>
+    </button>
   );
 }
