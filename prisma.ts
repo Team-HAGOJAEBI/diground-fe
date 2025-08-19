797d1c1954ebe1a68c7211c5e32de1be1233d0dc
chb6734 <chb6734@ack.co.kr>
2025-08-19 18:25:40 +0900

    feat: DB와 Mock을 선택할 수 있는 팩토리로  변경

--- prisma.ts
+++ prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+import { createPrismaClient } from "@/mocks/sample/Prisma-mock";
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
