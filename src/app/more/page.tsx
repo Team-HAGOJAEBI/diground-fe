6eceedf9379c14288c74862a58359983ed493375
chb6734 <chb6734@gmail.com>
2025-08-20 10:36:32 +0900

    feat: 로그아웃 페이지 추가

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import Image from "next/image";
+import { signOut, useSession } from "next-auth/react";
 
 import Footer from "@/app/_common/Footer";
 import Layout from "@/app/_common/Layout";
 
 export default function SettingPage() {
+  const { data } = useSession();
+
   return (
     <Layout className="relative flex h-screen flex-col overflow-hidden pb-[89px]">
       <div className="flex min-h-screen items-center justify-center">
+        <Image
+          src={data?.user?.image || ""}
+          alt="profile"
+          width={400}
+          height={400}
+          className="rounded-[16px]"
+        />
         <button
           className="bg-gray-60 rounded px-6 py-3"
           onClick={() => signOut()}
         >
-          Sign out
+          {data?.user?.name} Sign out
         </button>
       </div>
       <Footer selectedIndex={0} />
