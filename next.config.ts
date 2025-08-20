c78b9221fd043d876e56734b8c6458bf89fc006b
chb6734 <chb6734@gmail.com>
2025-08-21 03:04:35 +0900

    chore: 이미지 추가

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: "", // 사용하지 않으면 빈 문자열
         pathname: "/**", // 서브 경로 전체 허용 (필터링이 필요하면 세부 경로로 좁혀도 됨)
       },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "", // 사용하지 않으면 빈 문자열
+        pathname: "/**", // 서브 경로 전체 허용 (필터링이 필요하면 세부 경로로 좁혀도 됨)
+      },
     ],
     domains: ["image.bugsm.co.kr"],
   },
