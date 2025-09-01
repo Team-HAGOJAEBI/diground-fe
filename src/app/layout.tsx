a6fb6b16cc4e1ab220d28de0a6654bd42325d5fb
chb6734 <chb6734@gmail.com>
2025-09-02 00:05:46 +0900

    style: 레이아웃 Roboto font 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "../assets/styles/globals.css";
 
+import { Roboto } from "next/font/google";
 import localFont from "next/font/local";
 
 import { MSWProvider } from "./providers";
@@ -12,6 +13,12 @@ const Pretendard = localFont({
   variable: "--font-pretendard",
 });
 
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="ko"
-      className={Pretendard.className}
+      className={`${Pretendard.className} ${roboto.className}`}
     >
       <body>
         <MSWProvider>{children}</MSWProvider>
